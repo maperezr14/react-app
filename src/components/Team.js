8579f7dd6441a4fcbb4a25a94cd80fc9033cd042
milarte1 <116848503+milarte1@users.noreply.github.com>
2023-01-30 14:50:51 +0100

    primera pruba de la pagina

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,8 +1,50 @@
 import './../styles/css/main.css';
-
 function Team() {
     return(
-        <p>Aqui va el HTML de "Nuestro Equipo"</p>
+      <section className="team">
+        <div className="container">
+            <h2>Nuestro Equipo</h2>
+         <div className='fotos-equipo'>
+            <div className="fotos">
+            <img className="imagen-redonda" src="./images/1672749399307.jpg" alt=""></img>
+            <p>SILVIA PINO </p>
+            <p> FULLSTACK DEVELOPER</p>
+
+          </div>  
+          <div className="fotos">
+            <img className="imagen-redonda" src="./images/18221255.jpg" alt=""></img>
+            <p> MARI ANGELES</p>
+           <p>FULLSTACK DEVELOPER</p>
+
+
+          </div>  
+          <div className="fotos">
+            <img  className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+            <p>VALERY  SUAREZ</p>
+            <p>FULLSTACK DEVELOPER</p>
+
+
+          </div>  
+          <div className="fotos">
+            <img className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+             <p> MILA GALIANO</p>
+             <p>FULLSTACK DEVELOPER</p>
+
+          </div>  
+          <div className="fotos">
+            <img className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+            <p>KIARA MALASQUEZ</p>
+            <p>FULLSTACK DEVELOPER</p>
+          </div>  
+          <div className="fotos">
+            <img className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+            <p>FÁTIMA GARCIA FERNANDEZ</p>
+            <p>FULLSTACK DEVELOPER</p> 
+          </div>  
+
+          </div>     
+        </div>
+      </section>
     )
 }  
 
