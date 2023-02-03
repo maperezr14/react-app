749846e1c47953ac5ae56b5e04f67f470e334147
ValerySuarez22 <valerysuarez2296@gmail.com>
2023-02-03 13:36:29 +0100

    cambios

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,9 +1,60 @@
-import './../styles/css/main.css';
+import "./../styles/css/main.css";
 
-function Clients() {
-    return(
-        <p>Aqui va el HTML de "Clientes"</p>
-    )
-}  
-
-export default Clients;
+    function Clients() {
+    return (
+        <section>
+        <div className="container">
+            <h2>NUESTROS CLIENTES</h2>
+            <p>Nuestros clientes nos avalan.<br></br> 
+            Pensamos que la mejor carta depresentación de una empresa son sus clientes. En todo momento
+            ofrecemos un servicio personalizado y de calidad. Los clientes
+            satisfechos siguen día a día confiando en nuestra profesionalidad.
+            Desde el primer momento trabajamos en unión con ellos aportando
+            nuestra experiencia en todo el proceso.
+            </p>
+            <div className="containerClients">
+            <div className="sliderClients">
+                <div className="imgSlider">
+                <img src="../images/seat.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/ibera.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/asturiana.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/cocacola.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/wolkswagen.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/nestle.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/seat.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/ibera.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/asturiana.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/cocacola.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/wolkswagen.png" alt="m"></img>
+                </div>
+                <div className="imgSlider">
+                <img src="../images/nestle.png" alt="m"></img>
+                </div>
+                
+            </div>
+            </div>
+        </div>
+        </section>
+    );
+    }
+    export default Clients;
