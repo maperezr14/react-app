7c5ca3e942f669a1fa8436f79a8da0d98fd5f1e4
milarte1 <116848503+milarte1@users.noreply.github.com>
2023-02-03 13:34:25 +0100

    ultimos cambios

--- src/components/Team.js
+++ src/components/Team.js
@@ -6,7 +6,7 @@ function Team() {
             <h2>Nuestro Equipo</h2>
          <div className='fotos-equipo'>
             <div className="fotos">
-            <img className="imagen-redonda" src="./images/1672749399307.jpg" alt=""></img>
+            <img className="imagen-redonda" src="./images/Ellipse 9.jpg" alt=""></img>
             <p>SILVIA PINO </p>
             <p> FULLSTACK DEVELOPER</p>
 
@@ -19,25 +19,25 @@ function Team() {
 
           </div>  
           <div className="fotos">
-            <img  className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+            <img  className="imagen-redonda" src="./images/Rectangle 38.png" alt=""></img>
             <p>VALERY  SUAREZ</p>
             <p>FULLSTACK DEVELOPER</p>
 
 
           </div>  
           <div className="fotos">
-            <img className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+            <img className="imagen-redonda" src="./images/Ellipse 6.jpg" alt=""></img>
              <p> MILA GALIANO</p>
              <p>FULLSTACK DEVELOPER</p>
 
           </div>  
           <div className="fotos">
-            <img className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+            <img className="imagen-redonda" id="kiara" src="./images/kiara.jpg" alt=""></img>
             <p>KIARA MALASQUEZ</p>
             <p>FULLSTACK DEVELOPER</p>
           </div>  
           <div className="fotos">
-            <img className="imagen-redonda" src="https://i.pinimg.com/originals/c1/65/8e/c1658ec8d98d9861ca20c7b90c100544.jpg" alt=""></img>
+            <img className="imagen-redonda" src="./images/fati.jpg" alt=""></img>
             <p>FÁTIMA GARCIA FERNANDEZ</p>
             <p>FULLSTACK DEVELOPER</p> 
           </div>  
