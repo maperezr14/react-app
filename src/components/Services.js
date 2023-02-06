a428fd05fc3a083d330afe41c20cc914d4336ee7
kiaramm10 <kiaramalasquez5@gmail.com>
2023-02-06 14:18:15 +0100

    actuFinal

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,45 +1,64 @@
 import './../styles/css/main.css';
 
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const sliderSettings = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    arrows: false
+}
+
+const kiaServices = [ 
+    {   
+        subtitle: 'CAMPAÑAS PRINT',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nihil ex nesciunt quaerat fuga. Possimus nisi veritatis tenetur unde recusandae voluptatibus beatae enim, corporis iste, sunt maiores dolore accusamus sint!',
+        img: '../images/campPrint.svg',
+        movilImg: '../images/serv2.svg',
+
+    },
+    {
+        subtitle: 'CAMPAÑAS INTEGRALES',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nihil ex nesciunt quaerat fuga. Possimus nisi veritatis tenetur unde recusandae voluptatibus beatae enim, corporis iste, sunt maiores dolore accusamus sint!',
+        img: '../images/campInte.svg',
+        movilImg: '../images/serv3.svg',
+    
+    },
+    {
+        subtitle: 'CAMPAÑAS DIGITALES',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nihil ex nesciunt quaerat fuga. Possimus nisi veritatis tenetur unde recusandae voluptatibus beatae enim, corporis iste, sunt maiores dolore accusamus sint!',
+        img: '../images/campDigi.svg',
+        movilImg: '../images/serv1.svg',
+        
+    }
+]
+
 function Services() {
-    return(
-        
-        <section className='sectionServices'>
-             <div className='container'>
-                    <h2>NUESTROS SERVICIOS</h2>
-            <div className='firstGroup'>
-                <ul className='sliderOne'>
-                    <li id="slide1">
-                        <img className='desktopVer' src="./images/campPrint.svg" alt="anuncios en los metros"></img>
-                        <img className='movilVer' src="./images/serv2.svg" alt="anuncios en los metros"></img>
-                        <h3>CAMPAÑAS  PRINT</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nihil ex nesciunt quaerat fuga. Possimus nisi veritatis tenetur unde recusandae voluptatibus beatae enim, corporis iste, sunt maiores dolore accusamus sint!</p> </li>
-                    <li id="slide2">
-                        <img className='desktopVer' src="./images/campInte.svg" alt="imagen de hombre en una caminadora mirando un movil"></img>
-                        <img className='movilVer' src="./images/serv3.svg" alt="anuncios en los metros"></img>
-                        <h3>CAMPAÑAS  INTEGRALES</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nihil ex nesciunt quaerat fuga. Possimus nisi veritatis tenetur unde recusandae voluptatibus beatae enim, corporis iste, sunt maiores dolore accusamus sint!</p>
-                    </li>
-                    <li id="slide3">
-                        <img  className='desktopVer' src="./images/campDigi.svg" alt="anuncio LG"></img> 
-                        <img className='movilVer' src="./images/serv1.svg" alt="anuncios en los metros"></img>
-                        <h3>CAMPAÑAS DIGITALES</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nihil ex nesciunt quaerat fuga. Possimus nisi veritatis tenetur unde recusandae voluptatibus beatae enim, corporis iste, sunt maiores dolore accusamus sint!</p>
-                    </li>
-                 </ul>
+    return( 
+        <section className='section sectionServices'>
+            <div className='container'>
+                <h2>NUESTROS SERVICIOS</h2>
+                <Slider {...sliderSettings} className='sliderServices'>
+                    {kiaServices.map((kiaServices, index) => (
+                        <div key={index} className='itemSlider'>               
+                            <picture>
+                                <source alt={kiaServices.subtitle} srcSet={kiaServices.movilImg} media="(max-width:767px)"/>
+                                <img alt={kiaServices.subtitle} src={kiaServices.img} width="200px"/>                            
+                            </picture>
+                            <div className='caption'>
+                                <h3>{kiaServices.subtitle}</h3>
+                                <p>{kiaServices.content}</p>
+                            </div>
+                    </div>
+                ))}
+                </Slider>
             </div>
-            
-            <nav>
-                <ul className='menu'>
-                    <li className='button'><a href="#slide1">_</a></li>
-                    <li className='button'><a href="#slide2">_</a></li>
-                    <li className='button'><a href="#slide3">_</a></li>
-            </ul>
-            </nav>
-        </div>
-            
         </section>
-        
     )
-}  
+} 
+
 
 export default Services;
