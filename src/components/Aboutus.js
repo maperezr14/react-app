3a17fc9aabdcf816ed42ae0d88b5bf0ef73b5bc3
SpaceOddity7 <pino.gomez.silvi@gmail.com>
2023-02-07 12:04:46 +0100

    carga inicial Silvia

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -2,7 +2,21 @@ import './../styles/css/main.css';
 
 function Aboutus() {
     return(
-        <p>Aqui va el HTML de "Sobre Nosotros"</p>
+        <section>
+            <div className="container">
+                <h2>SOBRE NOSOTRAS</h2>
+                <div className='textoimagen'>
+                    <div className="textoprinp">
+                        <p> Somos una agencia especializada en marketing, publicidad y desarrollo web.
+                        Llevamos más de 25 años ayudando a nuestros clientes a desarrollar su marca personal.</p><p>Esta trayectora profesional nos permite anticiparnos a las últimas tendencias del mercado.
+                        Ogilvy está compuesto por profesionales de amplia experiencia en todos los ámbitos.</p>
+                    </div>
+                    <div className="img_sobrenosotros">
+                        <img src='./images/sobrenosotros.png' alt='Imagen-ogilvy'></img>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }  
 
